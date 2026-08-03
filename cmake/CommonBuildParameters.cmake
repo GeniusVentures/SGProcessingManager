c023da16d913a0ded92ef9204eb9366c2874aa89
itsafuu <sariru@gmail.com>
2026-08-03 17:45:40 -0400

    Fix non-android

--- cmake/CommonBuildParameters.cmake
+++ cmake/CommonBuildParameters.cmake
@@ -43,14 +43,11 @@ if(NOT TARGET Vulkan::Vulkan)
     find_package(Vulkan REQUIRED)
 endif()
 
-# On Android, override Vulkan::Vulkan to use our vendored Vulkan-Headers
-# instead of the NDK's system headers (see matching comment in
-# SuperGenius/build/CommonBuildParameters.cmake).
-if(ANDROID)
-    set_target_properties(Vulkan::Vulkan PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${_THIRDPARTY_BUILD_DIR}/Vulkan-Headers/include"
-    )
-endif()
+# Override Vulkan::Vulkan to use our vendored Vulkan-Headers on all platforms
+# (see matching comment in SuperGenius/build/CommonBuildParameters.cmake).
+set_target_properties(Vulkan::Vulkan PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${_THIRDPARTY_BUILD_DIR}/Vulkan-Headers/include"
+)
 
 # for compression, we need snappy
 set(Snappy_DIR "${_THIRDPARTY_BUILD_DIR}/snappy/lib/cmake/Snappy")
