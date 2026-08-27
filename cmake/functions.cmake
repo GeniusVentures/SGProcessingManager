4ccff19ffa4c3585759ea7f21bb5422594a44c20
Eduardo Menges Mattje <eduardomattje@gmail.com>
2026-08-27 12:56:08 -0300

    Deleted conflicting `addtest` CMake function

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -5,43 +5,6 @@ function(disable_clang_tidy target)
       )
 endfunction()
 
-function(addtest test_name)
-  add_executable(${test_name} ${ARGN})
-  addtest_part(${test_name} ${ARGN})
-  target_link_libraries(${test_name}
-      GTest::gtest_main
-      GTest::gmock_main
-      )
-  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/xunit)
-  set(xml_output "--gtest_output=xml:${CMAKE_BINARY_DIR}/xunit/xunit-${test_name}.xml")
-  add_test(
-      NAME ${test_name}
-      COMMAND $<TARGET_FILE:${test_name}> ${xml_output}
-  )
-  set_target_properties(${test_name} PROPERTIES
-      RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/test_bin
-      ARCHIVE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
-      LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
-      )
-  disable_clang_tidy(${test_name})
-
-  if(FORCE_MULTILE)
-    set_target_properties(${test_name} PROPERTIES LINK_FLAGS "${MULTIPLE_OPTION}")
-  endif()
-endfunction()
-
-function(addtest_part test_name)
-  if (POLICY CMP0076)
-    cmake_policy(SET CMP0076 NEW)
-  endif ()
-  target_sources(${test_name} PUBLIC
-      ${ARGN}
-      )
-  target_link_libraries(${test_name}
-      GTest::gtest
-      )
-endfunction()
-
 # conditionally applies flag.
 function(add_flag flag)
   check_cxx_compiler_flag(${flag} FLAG_${flag})
